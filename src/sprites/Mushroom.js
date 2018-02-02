3563bd6871b45015535ac85eec420d12adc22ae1
franci <franci@aktiviraj-se.net>
2018-02-02 09:52:42 +0100

    Initial

--- src/sprites/Mushroom.js
+++ src/sprites/Mushroom.js
@@ -1,12 +1,28 @@
 import Phaser from 'phaser'
 
 export default class extends Phaser.Sprite {
-  constructor ({ game, x, y, asset }) {
-    super(game, x, y, asset)
+  constructor ({ game, x, y, asset, name }) {
+    super(game, x, y, asset, name)
     this.anchor.setTo(0.5)
+    
+    // enabla inpute na objektu
+    this.inputEnabled = true;
   }
 
   update () {
     this.angle += 1
+    
+    // on mouse click izvede funkcijo onClick
+    this.events.onInputDown.add(onClick, this);
   }
+
 }
+
+function onClick() {
+
+ // alert("Clickec");
+ 
+ // izvede animacijo postition y, speed  ... absolutne pozicije
+ this.game.add.tween(this.body).to({ y: 300 }, 200, Phaser.Easing.Linear.None, true);
+}
+
--- src/states/Game.js
+++ src/states/Game.js
@@ -7,29 +7,60 @@ export default class extends Phaser.State {
   preload () {}
 
   create () {
+  
+    // textov ne rabimo
+    /*
     const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
+    
+     
+      let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText, {
       font: '40px Bangers',
       fill: '#77BFA3',
       smoothed: false
     })
+      
 
     banner.padding.set(10, 16)
     banner.anchor.setTo(0.5)
+    */
 
-    this.mushroom = new Mushroom({
-      game: this.game,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
 
-    this.game.add.existing(this.mushroom)
+    // inicializira igro za premike - za animacijo premika objekta ne rabimo 
+    // game.physics.startSystem(Phaser.Physics.ARCADE);
+
+
+
+    // doda nekaj spritov gor 
+    //
+    // nisem nasel kako dodati relativne koordinate glede na width oz. height objekta
+
+    for (let i = 0; i < 5; i++) {
+      this.mushroom = new Mushroom({
+        game: this.game,
+        x: 50 + i*100,                 
+        y: 50,
+        asset: 'mushroom',
+      }
+    )
+  
+      this.game.add.existing(this.mushroom);
+
+      //enabla physics na novemu objektu
+      game.physics.arcade.enable(this.mushroom);
+
+    }
   }
 
+
+
   render () {
+    
+    // ne rabimo izpisovanja parametrov
+    /*
     if (__DEV__) {
       this.game.debug.spriteInfo(this.mushroom, 32, 32)
     }
+    */
+
   }
 }
